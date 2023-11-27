152274b2861bbf885f1eaeba5184df46331a2917
CodeSandbox <support@codesandbox.io>
2023-11-27 17:05:52 +0000

    Initial commit

--- src/controllers/pdf-ops.js
+++ src/controllers/pdf-ops.js
@@ -1,9 +1,14 @@
 const fs = require("fs");
 const PDFExtract = require("pdf.js-extract").PDFExtract;
+let buffer;
 
-function samplePDFReader(_, res) {
+async function samplePDFReader(_, res) {
   const pdfExtract = new PDFExtract();
-  const buffer = fs.readFileSync("../public/docs/s1.pdf");
+  try {
+    buffer = await fs.readFileSync("./s1.pdf", "utf8");
+  } catch (err) {
+    console.error(err);
+  }
   const options = {}; /* see below */
   pdfExtract.extractBuffer(buffer, options, (err, data) => {
     if (err) return console.log(err);
